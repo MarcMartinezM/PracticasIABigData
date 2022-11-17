17a3861c5e508ed9900d08967531fdf1e652895e
Marc Martinez <m.martinez.moles00@gmail.com>
2022-11-17 18:52:51 +0100

    commit practica fashion

---
+++ base Datos/Ex2DML.sql
@@ -0,0 +1,5 @@
+select * from PROVEEDOR p, PRODUCTOS p2  where p.poblacion like 'Barcelona' AND p2.nombre like 'condensador';
+
+select * from PRODUCTOS p ,PROVEEDOR p2  WHERE p2.nombre like 'Electrònica Vivas';
+
+select * from CLIENTES c , FACTURA f  WHERE;
